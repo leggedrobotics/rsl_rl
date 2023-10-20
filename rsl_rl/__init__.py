1279190a1b182c08a9197d3ca18ba6ff4daf2ae2
Mayank Mittal <mittalma@leggedrobotics.com>
2023-10-20 15:46:07 -0400

    updates version to 2.0

--- rsl_rl/__init__.py
+++ rsl_rl/__init__.py
@@ -1,2 +1,7 @@
 #  Copyright 2021 ETH Zurich, NVIDIA CORPORATION
 #  SPDX-License-Identifier: BSD-3-Clause
+
+"""Main module for the rsl_rl package."""
+
+__version__ = "2.0.0"
+__license__ = "BSD-3"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rsl_rl",
-    version="1.0.2",
+    version="2.0.0",
     packages=find_packages(),
     license="BSD-3",
     description="Fast and simple RL algorithms implemented in pytorch",
