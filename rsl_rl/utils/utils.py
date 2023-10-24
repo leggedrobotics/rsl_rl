222fb1520d1068418986f3e63d550222929ede2a
Nikita Rudin <nrudin@nvidia.com>
2023-10-24 17:09:01 +0200

    fix to split and pad

--- rsl_rl/utils/utils.py
+++ rsl_rl/utils/utils.py
@@ -37,7 +37,12 @@ def split_and_pad_trajectories(tensor, dones):
     trajectory_lengths_list = trajectory_lengths.tolist()
     # Extract the individual trajectories
     trajectories = torch.split(tensor.transpose(1, 0).flatten(0, 1), trajectory_lengths_list)
+    # add at least one full length trajectory
+    trajectories = trajectories + (torch.zeros(tensor.shape[0], tensor.shape[-1], device=tensor.device), )
+    # pad the trajectories to the length of the longest trajectory
     padded_trajectories = torch.nn.utils.rnn.pad_sequence(trajectories)
+    # remove the added tensor
+    padded_trajectories = padded_trajectories[:, :-1]
 
     trajectory_masks = trajectory_lengths > torch.arange(0, tensor.shape[0], device=tensor.device).unsqueeze(1)
     return padded_trajectories, trajectory_masks
