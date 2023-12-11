8804e4f7306535e8416a3239b09fb8441a4e8960
Mayank Mittal <mittalma@leggedrobotics.com>
2023-12-11 18:55:06 +0100

    bumps version

--- rsl_rl/utils/utils.py
+++ rsl_rl/utils/utils.py
@@ -65,15 +65,21 @@ def store_code_state(logdir, repositories) -> list:
     for repository_file_path in repositories:
         try:
             repo = git.Repo(repository_file_path, search_parent_directories=True)
-        except git.InvalidGitRepositoryError:
+        except Exception:
+            print(f"Could not find git repository in {repository_file_path}. Skipping.")
             # skip if not a git repository
             continue
         # get the name of the repository
         repo_name = pathlib.Path(repo.working_dir).name
         t = repo.head.commit.tree
         diff_file_name = os.path.join(git_log_dir, f"{repo_name}.diff")
-        content = f"--- git status ---\n{repo.git.status()} \n\n\n--- git diff ---\n{repo.git.diff(t)}"
+        # check if the diff file already exists
+        if os.path.isfile(diff_file_name):
+            continue
+        # write the diff file
+        print(f"Storing git diff for '{repo_name}' in: {diff_file_name}")
         with open(diff_file_name, "x") as f:
+            content = f"--- git status ---\n{repo.git.status()} \n\n\n--- git diff ---\n{repo.git.diff(t)}"
             f.write(content)
         # add the file path to the list of files to be uploaded
         file_paths.append(diff_file_name)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rsl_rl",
-    version="2.0.1",
+    version="2.0.2",
     packages=find_packages(),
     author="ETH Zurich, NVIDIA CORPORATION",
     maintainer="Nikita Rudin, David Hoeller",
