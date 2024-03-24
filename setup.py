9c0fcdc67756d76645e7381fbef833c990a6508d
Lukas Schneider <32388635+e3ntity@users.noreply.github.com>
2024-03-24 12:25:56 -0700

    Update setup.py dependency versions

--- setup.py
+++ setup.py
@@ -8,13 +8,6 @@ setup(
     description="Fast and simple RL algorithms implemented in pytorch",
     python_requires=">=3.6",
     install_requires=[
-        "GitPython",
-        "gym[all]>=0.26.0",
-        "numpy>=1.24.4",
-        "onnx>=1.14.0",
-        "tensorboard>=2.10.0",
-        "torch>=1.10.0",
-        "torchvision>=0.5.0",
-        "wandb>=0.15.4",
+        "GitPython", "gym[all]", "numpy", "onnx", "tensorboard", "torch", "torchvision", "wandb",
     ],
 )
