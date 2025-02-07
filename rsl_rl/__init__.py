ac74a8bf2e5065136e994f1c8fc594d23d7cdac2
Clemens Schwarke <96480707+ClemensSchwarke@users.noreply.github.com>
2025-02-07 21:51:48 +0100

    Update license in all files and maintainers (#56)
    * update readme, contributors, etc.
    
    * license changes
    
    * add license header
    
    * update license
    
    * fix name and version

--- rsl_rl/__init__.py
+++ rsl_rl/__init__.py
@@ -1,7 +1,9 @@
-#  Copyright 2021 ETH Zurich, NVIDIA CORPORATION
-#  SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021-2025, ETH Zurich and NVIDIA CORPORATION
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
 
 """Main module for the rsl_rl package."""
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 __license__ = "BSD-3"
