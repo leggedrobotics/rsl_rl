e2cda9dba502d1254bf9237241c6879cddf46460
peisuke <peisuke.com@gmail.com>
2025-03-18 15:49:41 +0900

    Refactor: Use `self.device` for model loading in `torch.load()` to ensure consistency.

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -219,7 +219,7 @@ class OnPolicyRunner:
             }, path)
 
     def load(self, path, load_optimizer=True):
-        loaded_dict = torch.load(path)
+        loaded_dict = torch.load(path, map_location=torch.device(self.device))
         self.alg.actor_critic.load_state_dict(loaded_dict['model_state_dict'])
         if load_optimizer:
             self.alg.optimizer.load_state_dict(loaded_dict['optimizer_state_dict'])
