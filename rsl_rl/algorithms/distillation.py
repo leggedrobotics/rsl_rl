c6834de555500ad55f8a837c54ea77fc988c4e8a
alessandroassirelli98 <37577103+alessandroassirelli98@users.noreply.github.com>
2025-05-09 16:26:19 +0200

    Adds gradient cap for teacher student distillation (#91)
    This PR adds a gradient cap to the teacher-student distillation setup.
    The goal is to prevent excessively large gradients from destabilizing training.
    
    📌 Changes
    Introduced a clipping mechanism to cap the gradients during backpropagation in the distillation process.
    
    Helps improve training stability, especially in early iterations.
    
    ---------
    
    Co-authored-by: alessandro.assirelli <alessandro.assirelli@agile-robots.com>

--- rsl_rl/algorithms/distillation.py
+++ rsl_rl/algorithms/distillation.py
@@ -25,6 +25,7 @@ class Distillation:
         num_learning_epochs=1,
         gradient_length=15,
         learning_rate=1e-3,
+        max_grad_norm=None,
         loss_type="mse",
         device="cpu",
         # Distributed training parameters
@@ -55,6 +56,7 @@ class Distillation:
         self.num_learning_epochs = num_learning_epochs
         self.gradient_length = gradient_length
         self.learning_rate = learning_rate
+        self.max_grad_norm = max_grad_norm
 
         # initialize the loss function
         if loss_type == "mse":
@@ -127,6 +129,8 @@ class Distillation:
                     loss.backward()
                     if self.is_multi_gpu:
                         self.reduce_parameters()
+                    if self.max_grad_norm:
+                        nn.utils.clip_grad_norm_(self.policy.student.parameters(), self.max_grad_norm)
                     self.optimizer.step()
                     self.policy.detach_hidden_states()
                     loss = 0
